cd0fe27db4e0d04f909e59e9d92eb511a1073e4f
ontheway30 <445044876@qq.com>
2020-05-20 16:39:17 +0800

    Update Program.vb
    nice

--- ConsoleApp2/Program.vb
+++ ConsoleApp2/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World! Ä¿Ç°ÔÚmaster·ÖÖ§ µÍ   edit in remote rep")
+        Console.WriteLine("Hello World! Ä¿Ç°ÔÚmaster·ÖÖ§ µÍ   edit in remote rep  哈哈哈哈哈哈哈哈哈")
     End Sub
 End Module
