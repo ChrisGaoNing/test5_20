0b63faaa9ec3893ae13df239b5b7c7164af1d8a8
gn <445044876@qq.com>
2020-05-20 16:54:10 +0800

    本地提交第二次

--- ConsoleApp2/Program.vb
+++ ConsoleApp2/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World! Ä¿Ç°ÔÚmaster·ÖÖ§ µÍ   edit in remote rep  哈哈哈哈哈哈哈哈哈")
+        Console.WriteLine("Hello World! Ä¿Ç°ÔÚmaster·ÖÖ§ µÍ   edit in remote rep")
     End Sub
 End Module
